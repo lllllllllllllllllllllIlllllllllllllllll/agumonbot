85c25b09ec28581362c04aca7ec2c1255ee6205d
lllllllllllllllllllllIlllllllllllllllll <111403501+lllllllllllllllllllllIlllllllllllllllll@users.noreply.github.com>
2022-11-16 12:34:20 +1300

    initial commit

---
+++ AgumonBot/Modules/Commands.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using System.Text.Json;
+
+namespace AgumonBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        Random rnd = new Random();
+        [Command("8ball")]
+        public async Task YesNo([Remainder] string query)
+        {
+            string[] answers = File.ReadAllLines("8ball.txt");
+            await ReplyAsync("*" + answers[rnd.Next(0, answers.Length)] + "*");
+
+        }
+
+        [Command("roll")]
+        public async Task RollDie([Remainder] int num)
+        {
+            await ReplyAsync("You have rolled: " + rnd.Next(1, num + 1).ToString());
+        }
+
+        [Command("bandai")]
+        public async Task Bandai([Remainder] string query)
+        {
+            string format = "https://www.bandai-tcg-plus.com/card?card_param={0}&game_title_id=2&limit=20&offset=0";
+            await ReplyAsync(string.Format(format, query));
+        }
+
+        [Command("card")]
+        public async Task Card([Remainder] string query)
+        {
+            string output = "";
+
+            if (query.Substring(0,2).ToUpper() == "BT")
+            {
+                query = query.Substring(0, 2).ToUpper() + query.Substring(2);
+                string format = "https://digimoncardgame.fandom.com/wiki/{0}";
+                output = string.Format(format, query);
+            }
+            else if (query.Split('-').Length > 1)
+            {
+                string format = "https://digimoncardgame.fandom.com/wiki/BT{0}";
+                output = string.Format(format, query);
+            }
+            else
+            {
+                var jsonFile = File.ReadAllText("database.json");
+            }
+            await ReplyAsync(output);
+        }
+
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("pong");
+        }
+
+        [Command("pong")]
+        public async Task Pong()
+        {
+            await ReplyAsync("ping");
+        }
+
+        [Command("timetable")]
+        public async Task ShowTimetable()
+        {
+            await ReplyAsync("https://media.discordapp.net/attachments/884943555687239743/999478049387073616/BIT-2022-V4.png?width=1402&height=701");
+        }
+
+        [Command("help")]
+        public async Task ListCommands()
+        {
+            string output = "```" +
+                "digimon\n" +
+                "------------\n" + 
+                "!bandai cardname: looks up digimon card by name in bandai tcgplus\n" +
+                "!card {set-number}: looks up digimon cards, format: !card bt6-111 or !card 6-111\n" +
+                "school+\n" +
+                "------------\n" +
+                "!timetable: shows a picture of timetable\n" +
+                "fun\n" + 
+                "------------\n" +
+                "!8ball {query}: consult the all knowing 8ball\n" +
+                "!roll x: roll x sided die\n" +
+                "!ping: pong" +
+                "```";
+            await ReplyAsync(output);
+        }
+    }
+}
---
+++ AgumonBot/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AgumonBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            // subscribe logging handler to both the client and the CommandService
+            _client.Log += Log;
+            _commands.Log += Log;
+
+            // Setup your DI container
+            _services = ConfigureServices(_client, _commands);
+
+            string token = File.ReadAllText("token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await InitCommands();
+
+            // Block this task until the program is closed.
+            await Task.Delay(Timeout.Infinite);
+        }
+        
+        private static IServiceProvider ConfigureServices
+            (DiscordSocketClient client, CommandService commands)
+        {
+            var map = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(commands);
+
+            return map.BuildServiceProvider();
+        }
+
+        public async Task InitCommands()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var msg = arg as SocketUserMessage;
+
+            // bail out if system message
+            if (msg == null) return;
+
+            // do not respond to other bots or itself
+            if (msg.Author.IsBot) return;
+
+            int pos = 0;
+
+            // ! prefix
+            if (msg.HasCharPrefix('!', ref pos))
+            {
+                // create a command context
+                var context = new SocketCommandContext(_client, msg);
+
+                // execute command
+                var result = await _commands.ExecuteAsync(context, pos, _services);
+
+                /*
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+                    await msg.Channel.SendMessageAsync(result.ErrorReason);
+                }
+                */
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            switch(msg.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine(msg.ToString());
+            Console.ResetColor();
+            return Task.CompletedTask;
+        }
+    }
+}
